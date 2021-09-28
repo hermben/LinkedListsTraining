fd618072e98286ff080b1be954db08136d5d4b86
hermben <hermben@gmail.com>
2021-09-28 16:38:13 -0700

    adds corrections

--- LinkedListsTraining/Program.cs
+++ LinkedListsTraining/Program.cs
@@ -6,7 +6,22 @@ namespace LinkedListsTraining
     {
         static void Main(string[] args)
         {
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            Console.WriteLine("Given Array");
+            printArray(arr);
+            Sort.MergeSort(arr);
+            Console.WriteLine("\nSorted array");
+            printArray(arr);
+        }
 
+        // A utility function to
+        // print array of size n */
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
         }
     }
 }
